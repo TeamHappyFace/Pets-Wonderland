bd04f7365715b9c7b944142990dd0adc224b7145
Hristina Ilieva <xristina.i.ilieva@gmail.com>
2017-02-03 16:28:36 +0200

    remove userService

---
+++ PetsWonderland/Tests/Business/PetsWonderland.Business.Tests/Services/Mocks/MockedAnimalService.cs
@@ -0,0 +1,11 @@
+﻿using PetsWonderland.Business.Data.Contracts;
+using PetsWonderland.Business.Models.Animals;
+using PetsWonderland.Business.Services;
+
+namespace PetsWonderland.Business.Tests.Services.Mocks
+{
+	internal class MockedAnimalService
+	{
+
+	}
+}
