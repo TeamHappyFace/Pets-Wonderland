dad20de20521684fcfe50abda1af3b84b7b0548c
Dimitar Kalaydzhiev <dimitar.kalaydzhievv@gmail.com>
2017-02-07 23:52:37 +0200

    Added user Models tests (100% coverage)

---
+++ PetsWonderland/Tests/Business/PetsWonderland.Models.Tests/UsersTests/HotelManagerTests.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using PetsWonderland.Business.Models.Hotels;
+using PetsWonderland.Business.Models.Requests;
+using PetsWonderland.Business.Models.Users;
+
+namespace PetsWonderland.Models.Tests.UsersTests
+{
+    [TestFixture]
+    public class HotelManagerTests
+    {
+        [Test]
+        public void Constructor_ShouldHaveParameterlessConstructor()
+        {
+            // Arrange & Act
+            var manager = new HotelManager();
+
+            // Assert
+            Assert.IsInstanceOf<HotelManager>(manager);
+        }
+
+        [Test]
+        public void Constructor_ShouldInitializeBoardingRequestsCollectionCorrectly()
+        {
+            var manager = new HotelManager();
+
+            var boardingRequests = manager.UserBoardingRequests;
+
+            Assert.That(boardingRequests, Is.Not.Null.And.InstanceOf<HashSet<UserBoardingRequest>>());
+        }
+
+        [Test]
+        public void Constructor_ShouldInitializeHotelsCollectionCorrectly()
+        {
+            var manager = new HotelManager();
+
+            var hotels = manager.Hotels;
+
+            Assert.That(hotels, Is.Not.Null.And.InstanceOf<HashSet<Hotel>>());
+        }
+
+        [TestCase(123)]
+        [TestCase(12)]
+        public void BoardingRequestCollection_ShouldGetAndSetDataCorrectly(int testId)
+        {
+            var boardingRequest = new UserBoardingRequest() { Id = testId };
+            var set = new HashSet<UserBoardingRequest> { boardingRequest };
+
+            var manager = new HotelManager { UserBoardingRequests = set };
+
+            Assert.AreEqual(manager.UserBoardingRequests.First().Id, testId);
+        }
+
+        [TestCase(123)]
+        [TestCase(12)]
+        public void HotelsCollection_ShouldGetAndSetDataCorrectly(int testId)
+        {
+            var hotel = new Hotel { Id = testId };
+            var set = new HashSet<Hotel> { hotel };
+
+            var manager = new HotelManager { Hotels = set };
+
+            Assert.AreEqual(manager.Hotels.First().Id, testId);
+        }
+
+        [TestCase(true)]
+        [TestCase(false)]
+        public void IsDeleted_ShouldGetAndSetDataCorrectly(bool testIsDeleted)
+        {
+            // Arrange & Act
+            var manager = new HotelManager { IsDeleted = testIsDeleted };
+
+            //Assert
+            Assert.AreEqual(manager.IsDeleted, testIsDeleted);
+        }
+    }
+}
---
+++ PetsWonderland/Tests/Business/PetsWonderland.Models.Tests/UsersTests/UserTests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using PetsWonderland.Business.Models.Animals;
+using PetsWonderland.Business.Models.Requests;
+using PetsWonderland.Business.Models.Users;
+
+namespace PetsWonderland.Models.Tests.UsersTests
+{
+    [TestFixture]
+    public class UserTests
+    {
+        [Test]
+        public void Constructor_ShouldHaveParameterlessConstructor()
+        {
+            // Arrange & Act
+            var user = new User();
+
+            // Assert
+            Assert.IsInstanceOf<User>(user);
+        }
+
+        [Test]
+        public void Constructor_ShouldInitializeAnimalsCollectionCorrectly()
+        {
+            var user = new User();
+
+            var animals = user.Animals;
+
+            Assert.That(animals, Is.Not.Null.And.InstanceOf<HashSet<UserAnimal>>());
+        }
+
+        [TestCase(123)]
+        [TestCase(6)]
+        public void AnimalsCollection_ShouldGetAndSetDataCorrectly(int userAnimalId)
+        {
+            var animal = new UserAnimal() { Id = userAnimalId };
+            var set = new HashSet<UserAnimal> { animal };
+
+            var user = new User { Animals = set };
+
+            Assert.AreEqual(user.Animals.First().Id, userAnimalId);
+        }
+
+        [TestCase(true)]
+        [TestCase(false)]
+        public void IsDeleted_ShouldGetAndSetDataCorrectly(bool testIsDeleted)
+        {
+            // Arrange & Act
+            var user = new User { IsDeleted = testIsDeleted };
+
+            //Assert
+            Assert.AreEqual(user.IsDeleted, testIsDeleted);
+        }
+    }
+}
