473a5ec309692d7b350107b7a503dbb2d32ad9e0
Dimitar Kalaydzhiev <dimitar.kalaydzhievv@gmail.com>
2017-02-20 09:45:59 +0200

    Account cleanup Added login and registert design

--- PetsWonderland/Client/PetsWonderland.Client/Account/Register.aspx.designer.cs
+++ PetsWonderland/Client/PetsWonderland.Client/Account/Register.aspx.designer.cs
@@ -13,15 +13,6 @@ namespace PetsWonderland.Client.Account {
     public partial class Register {
         
         /// <summary>
-        /// ErrorMessage control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Literal ErrorMessage;
-        
-        /// <summary>
         /// UserType control.
         /// </summary>
         /// <remarks>
