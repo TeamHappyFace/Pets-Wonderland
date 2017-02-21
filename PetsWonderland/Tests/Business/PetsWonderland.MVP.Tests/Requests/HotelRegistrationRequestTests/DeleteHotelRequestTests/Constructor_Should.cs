75abcbb3474152196119c805efc97c5a09b54b30
Hristina Ilieva <xristina.i.ilieva@gmail.com>
2017-02-22 00:56:12 +0200

    add adapter

---
+++ PetsWonderland/Tests/Business/PetsWonderland.MVP.Tests/Requests/HotelRegistrationRequestTests/DeleteHotelRequestTests/Constructor_Should.cs
@@ -0,0 +1,38 @@
+﻿using Moq;
+using NUnit.Framework;
+using PetsWonderland.Business.MVP.Requests.HotelRegistrationRequest.DeleteHotelRequest;
+using PetsWonderland.Business.MVP.Requests.HotelRegistrationRequest.DeleteHotelRequest.ViewModels;
+using PetsWonderland.Business.MVP.Requests.HotelRegistrationRequest.DeleteHotelRequest.Views;
+using PetsWonderland.Business.Services.Contracts;
+
+namespace PetsWonderland.MVP.Tests.Requests.HotelRegistrationRequestTests.DeleteHotelRequestTests
+{
+	[TestFixture]
+	public class Constructor_Should
+	{
+		[Test]
+		public void CreatePresenter_WhenArgumentsAreValid()
+		{
+			var mockedHotelRequestView = new Mock<IDeleteHotelRequestView>();
+			var mockedHotelRequestService = new Mock<IHotelRegistrationRequestService>();
+
+			var mockedHotelRequestModel = new DeleteHotelRequestModel();
+			mockedHotelRequestView
+				.SetupGet(x => x.Model)
+				.Returns(mockedHotelRequestModel);
+
+			Assert.DoesNotThrow(() =>
+				new DeleteHotelRequestPresenter(mockedHotelRequestView.Object, mockedHotelRequestService.Object));
+		}
+
+		[Test]
+		public void ThrowArgumentNullException_WhenHotelRequestServiceIsNull()
+		{
+			var mockedHotelRequestView = new Mock<IDeleteHotelRequestView>();
+
+			Assert.That(() =>
+				new DeleteHotelRequestPresenter(mockedHotelRequestView.Object, null),
+				Throws.ArgumentNullException.With.Message.Contain("hotelRegistrationRequestService"));
+		}
+	}
+}
---
+++ PetsWonderland/Tests/Business/PetsWonderland.MVP.Tests/Requests/HotelRegistrationRequestTests/DeleteHotelRequestTests/DeleteHotelRegistrationRequest_Should.cs
@@ -0,0 +1,35 @@
+﻿using Moq;
+using NUnit.Framework;
+using PetsWonderland.Business.Models.Requests;
+using PetsWonderland.Business.MVP.Requests.HotelRegistrationRequest.DeleteHotelRequest;
+using PetsWonderland.Business.MVP.Requests.HotelRegistrationRequest.DeleteHotelRequest.Args;
+using PetsWonderland.Business.MVP.Requests.HotelRegistrationRequest.DeleteHotelRequest.ViewModels;
+using PetsWonderland.Business.MVP.Requests.HotelRegistrationRequest.DeleteHotelRequest.Views;
+using PetsWonderland.Business.Services.Contracts;
+
+namespace PetsWonderland.MVP.Tests.Requests.HotelRegistrationRequestTests.DeleteHotelRequestTests
+{
+	[TestFixture]
+	public class DeleteHotelRegistrationRequest_Should
+	{
+		[Test]
+		public void WorkProperly_WhenParamsAreValid()
+		{
+			var mockedHotelRequestView = new Mock<IDeleteHotelRequestView>();
+			var mockedHotelRequestService = new Mock<IHotelRegistrationRequestService>();
+
+			mockedHotelRequestView
+				.SetupGet(x => x.Model)
+				.Returns(new DeleteHotelRequestModel());
+
+			var deleteHotelRequestPresenter = new DeleteHotelRequestPresenter(mockedHotelRequestView.Object, mockedHotelRequestService.Object);
+
+			var hotelRequest = new Mock<UserHotelRegistrationRequest>();
+
+			mockedHotelRequestView.Raise(x => x.DeleteHotelRegistrationRequest += null,
+				new DeleteHotelRequestArgs(hotelRequest.Object.Id));
+
+			Assert.AreEqual(false, hotelRequest.Object.IsDeleted);
+		}
+	}
+}
